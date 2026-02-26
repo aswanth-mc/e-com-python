cf65367035987ddf9d7916f673e7748399d534a5
aswanth-mc <mcaswanth5@gmail.com>
2026-02-26 21:32:44 +0530

    refactor(model): add __str__ method for better model representation

--- MiniEcom/shop/models.py
+++ MiniEcom/shop/models.py
@@ -9,6 +9,9 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=15, blank=True)
     address = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 # product model
 class Product(models.Model):
     name = models.CharField(max_length=150)
@@ -16,11 +19,17 @@ class Product(models.Model):
     price =models.DecimalField(max_digits=10,decimal_places=2)
     stock = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 # order model
 class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
     order_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, default='Pending')
+
+    def __str__(self):
+        return self.name
 
 # order item model
 class OrderItem(models.Model):
@@ -28,5 +37,8 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
 
