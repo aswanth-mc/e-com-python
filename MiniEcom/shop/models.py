3f941dd2d34ca01598ae265eea39accad3a26ac7
aswanth-mc <mcaswanth5@gmail.com>
2026-02-26 21:08:40 +0530

    feat : ceate order and order item model

--- MiniEcom/shop/models.py
+++ MiniEcom/shop/models.py
@@ -16,3 +16,17 @@ class Product(models.Model):
     price =models.DecimalField(max_digits=10,decimal_places=2)
     stock = models.IntegerField()
 
+# order model
+class Order(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default='Pending')
+
+# order item model
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+
